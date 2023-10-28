8fa19363af62640e22202f53279ff81772feabdb
Abdulrahman-Abdulhak <bdalrhmanbdalhq@gmail.com>
2023-10-28 20:26:01 +0300

    Maram Kaiser and Raghad Al-Motallak

--- Binary_Search_Homework/BinarySearchHomework.cs
+++ Binary_Search_Homework/BinarySearchHomework.cs
@@ -14,6 +14,34 @@ namespace Binary_Search_Homework // don't edit this line!!!
                                                                                 // من الممكن تمرير قيمة 0 لتجاهلهم
         {
             //TODO: write code for the ternary search algorithm and return the index of the element
+            if (end >= start)
+            {
+                int mid1 = start + (end - 1) / 3;
+                int mid2 = end - (end - 1) / 3;
+                if (arr[mid1] == key)
+                {
+                    return mid1;
+
+                }
+                if (arr[mid2] == key)
+                {
+                    return mid2;
+                }
+                if (key < arr[mid1])
+                {
+                    return TernarySearch(arr, key, start, mid1 - 1);
+                }
+                else if (key > arr[mid2])
+                {
+                    return TernarySearch(arr, key, mid1 + 1, end);
+                }
+                else
+                {
+                    return TernarySearch(arr, key, mid1 + 1, mid2 - 1);
+                }
+
+
+            }
             return -1;
         }
 
@@ -31,7 +59,42 @@ namespace Binary_Search_Homework // don't edit this line!!!
         {
             //TODO: write code to calculate the repeat count of a spacific element
             // make sure to use the previous method in this method
-            return -1;
+            int count = 0;
+            int left = 0;
+            int right = arr.Length - 1;
+
+
+            while (left <= right)
+            {
+                int middle = (left + right) / 2;
+
+                if (arr[middle] == key)
+                {
+                    count++;
+                    break;
+                }
+                else if (arr[middle] < key)
+                {
+                    left = middle + 1;
+                }
+                else
+                {
+                    right = middle - 1;
+                }
+            }
+
+            if (count > 0)
+            {
+                int index = Array.IndexOf(arr, key);
+
+                while (index + 1 < arr.Length && arr[index + 1] == key)
+                {
+                    count++;
+                    index++;
+                }
+            }
+
+            return count;
         }
     }
 }
